1f4b3caf0400a679a7714cea85b0a586cb6600a0
csmatters <dapankaj@gmail.com>
2025-08-04 18:16:22 +0530

    Resolved the issue for the reposniveness

--- src/components/testimonial.jsx
+++ src/components/testimonial.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-function Testimonial({comment, author, designation}) {
+function Testimonial({ comment, author, designation }) {
   return (
-   
-       <div className="bg-gray-900 p-2 rounded-lg max-w-xl mx-auto text-white">
-            <div className="relative bg-white text-gray-800 text-base rounded-md p-6 shadow-md">
-                <p className="italic leading-relaxed">
-                    {comment}
-                </p>
-                {/* Arrow below the box */}
-                <div className="absolute bottom-[-10px] left-10 w-0 h-0 border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent border-t-[10px] border-t-white"></div>
-            </div>
+    <div className="bg-gray-900 p-4 sm:p-6 rounded-lg w-full max-w-md mx-auto text-white">
+      {/* Testimonial Card */}
+      <div className="relative bg-white text-gray-800 text-base rounded-md p-4 sm:p-6 shadow-md">
+        <p className="italic leading-relaxed">{comment}</p>
 
-            {/* Author Info */}
-            <div className="mt-6 ml-4">
-                <p className="text-orange-500 font-semibold">{author}</p>
-                <p className="text-gray-400">{designation}</p>
-            </div>
-        </div>
+        {/* Arrow */}
+        <div className="absolute -bottom-2 left-8 w-0 h-0 border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent border-t-[10px] border-t-white"></div>
+      </div>
 
-  )
+      {/* Author Info */}
+      <div className="mt-4 ml-2 sm:ml-4">
+        <p className="text-orange-500 font-semibold">{author}</p>
+        <p className="text-gray-400 text-sm">{designation}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Testimonial;
+
+
